6b1c299039a6ca095bde50208db9572aed39798b
Michael Donovan <michael@mdonovan.dev>
2023-06-19 20:48:27 -0500

    format flake.nix

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "<description>";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }@inputs:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        stdenv = pkgs.gcc13Stdenv;
+
+        nativeBuildInputs = with pkgs; [
+          cmake
+          ninja
+        ];
+      in
+
+      {
+        packages = with pkgs;{
+          pkg = stdenv.mkDerivation {
+            pname = "<name>";
+            version = "0.0.1";
+            src = ./.;
+            cmakeFlags = [
+              "-GNinja"
+            ];
+
+            nativeBuildInputs = nativeBuildInputs;
+            enableParallelBuilding = true;
+
+            meta = with lib; {
+              description = "<description>"
+                license = licenses.gpl3;
+              platforms = platforms.all;
+              maintainers = with maintainers; [ michaeldonovan ];
+            };
+          };
+        };
+
+        devShell = stdenv.mkDerivation {
+          name = "devShell";
+          nativeBuildInputs = [ pkgs.gdb nativeBuildInputs ];
+        };
+
+        defaultPackage = self.packages.${system}.pkg;
+      });
+}
